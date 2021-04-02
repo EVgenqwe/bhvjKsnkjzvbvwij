824d041d54b2e2fc511b546757dde4e42f18b069
EVgenqwe <81819102+EVgenqwe@users.noreply.github.com>
2021-04-02 18:00:58 +0300

    Add files via upload

---
+++ 2LB-3/2LB-3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _2LB_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("String:");
+            string InputString1 = Console.ReadLine();
+            string[] Words = InputString1.Split(' ');
+            for (int i = Words.Length - 1; i >= 0; i--)
+            {
+                Console.Write(Words[i] + " ");
+            }
+        }
+    }
+}

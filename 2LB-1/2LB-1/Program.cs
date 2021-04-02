20cb72949bb2798c8220052a6c94a30224662f00
EVgenqwe <81819102+EVgenqwe@users.noreply.github.com>
2021-04-02 17:59:07 +0300

    Add files via upload

---
+++ 2LB-1/2LB-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _2LB_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vvedite:");
+            string InputString2 = Console.ReadLine();
+            for (int i = 0; i < InputString2.Length; i++)
+            {
+                char SearchSymbol = InputString2[i];
+                if ((SearchSymbol >= 'А') && (SearchSymbol <= 'Я'))
+                {
+                    Console.WriteLine("Русская заглавная  буква:" + InputString2[i]);
+                }
+            }
+        }
+    }
+}
